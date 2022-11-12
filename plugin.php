069eba6c99f72884248df5d9d5dc5a60582d5644
mdibella-dev <mdb@marcodibella.de>
2022-11-12 17:50:52 +0100

    Change description and version

--- plugin.php
+++ plugin.php
@@ -1,11 +1,11 @@
 <?php
 /*
- * Plugin Name:     Marco Di Bella - Blocks
+ * Plugin Name:     Marco Di Bella - Blocks 2
  * Plugin URI:      https://github.com/mdibella-dev/mdb-theme-blocks
- * Description:     This plugin adds custom blocks to the Gutenberg editor for use in Marco Di Bella's personal WordPress theme (mdb-theme-fse).
+ * Description:     This plugin adds custom blocks to the Gutenberg editor for use in Marco Di Bella's personal WordPress theme (mdb-theme-fse). Based on mdb-theme-blocks (1.2.0).
  * Author:          Marco Di Bella
  * Author URI:      https://www.marcodibella.de
- * Version:         1.2.0
+ * Version:         1.0.0
  * Text Domain:     mdb-theme-blocks
  * Domain Path:     /languages
  *
@@ -24,7 +24,7 @@ defined( 'ABSPATH' ) or exit;
 
 /** Variables and definitions **/
 
-$plugin_version = '1.2.0';
+$plugin_version = '1.0.0';
 $plugin_path    = plugin_dir_path( __FILE__ );
 
 
