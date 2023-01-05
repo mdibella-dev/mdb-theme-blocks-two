cf9b0835e6cbebb3ea2d8b9eb93866ad0fa6b91d
Marco Di Bella <mdb@marcodibella.de>
2023-01-05 18:28:23 +0100

    Add Gutenberg check and consolidate function call.

--- includes/blocks/download-container/block.php
+++ includes/blocks/download-container/block.php
@@ -14,20 +14,24 @@ namespace mdb_theme_blocks;
 defined( 'ABSPATH' ) or exit;
 
 
-if ( function_exists( 'register_block_type' ) ) :
 
-    function register_block()
-    {
-        // Register the block by passing the location of block.json to register_block_type.
-        register_block_type_from_metadata(
-            __DIR__,
-            array(
-                'title'           => __( 'Download Container', PLUGIN_DOMAIN ),
-                'description'     => __( 'A container element for providing downloads.', PLUGIN_DOMAIN ),
-            )
-        );
-    }
 
-    add_action( 'init', 'mdb_theme_blocks\register_block' );
+function register_block_download_container()
+{
+    // Check if Gutenberg exist
+    if( ! function_exists( 'register_block_type_from_metadata' ) ) :
+        return;
+    endif;
 
-endif;
+
+    // Register block
+    register_block_type_from_metadata(
+        __DIR__,
+        array(
+            'title'           => __( 'Download Container', PLUGIN_DOMAIN ),
+            'description'     => __( 'A container element for providing downloads.', PLUGIN_DOMAIN ),
+        )
+    );
+}
+
+add_action( 'init', 'mdb_theme_blocks\register_block_download_container' );
