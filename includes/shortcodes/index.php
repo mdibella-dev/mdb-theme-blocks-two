9bf303d7560fa3d6c7b437a8513802feee2eb80d
Marco Di Bella <mdb@marcodibella.de>
2023-02-04 18:32:38 +0100

    Remove symple

---
+++ includes/shortcodes/index.php
@@ -0,0 +1,9 @@
+<?php
+/** Prevent direct access */
+
+defined( 'ABSPATH' ) or exit;
+
+
+require_once( 'shortcode-publikationsliste.php' );
+require_once( 'shortcode-vortragsliste.php' );
+require_once( 'shortcode-teaserblock.php' );
