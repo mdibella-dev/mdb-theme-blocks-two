2afab286a9a28ce6aa020d87e5a3c85ff495d639
Marco Di Bella <mdb@marcodibella.de>
2023-03-03 18:47:34 +0100

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@
  * Description:     This plugin adds custom blocks to the Gutenberg editor for use in Marco Di Bella's personal WordPress theme (mdb-theme-fse). Basically a replacement of mdb-theme-blocks (1.2.0) and mdb-theme-ajax (1.1.0).
  * Author:          Marco Di Bella
  * Author URI:      https://www.marcodibella.de
- * Version:         1.1.1
+ * Version:         1.1.2
  * Text Domain:     mdb-theme-blocks
  *
  * @author  Marco Di Bella
@@ -23,7 +23,7 @@ defined( 'ABSPATH' ) or exit;
 
 /** Variables and definitions **/
 
-define( __NAMESPACE__ . '\PLUGIN_VERSION', '1.1.1' );
+define( __NAMESPACE__ . '\PLUGIN_VERSION', '1.1.2' );
 define( __NAMESPACE__ . '\PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( __NAMESPACE__ . '\PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
@@ -34,6 +34,7 @@ define( __NAMESPACE__ . '\PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 require_once( PLUGIN_DIR . 'includes/classes/index.php' );
 require_once( PLUGIN_DIR . 'includes/shortcodes/index.php' );
 require_once( PLUGIN_DIR . 'includes/block-categories.php' );
+require_once( PLUGIN_DIR . 'includes/patches.php' );
 require_once( PLUGIN_DIR . 'includes/setup.php' );
 
 require_once( PLUGIN_DIR . 'blocks/index.php' );
