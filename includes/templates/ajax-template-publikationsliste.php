7aa9b4b7d1f7fa2cc4394afbc09b775a63bd2d1e
Marco Di Bella <mdb@marcodibella.de>
2023-06-12 19:03:47 +0200

    Add <p> tag

--- includes/templates/ajax-template-publikationsliste.php
+++ includes/templates/ajax-template-publikationsliste.php
@@ -16,7 +16,7 @@
             <p class="small">_PUBCITE_</p>
         </div>
         <div>
-            <a href="_LINK_" data-pubid="_ID_" data-action="pubdetails" target="_self">_DETAILS_</a>
+            <p><a href="_LINK_" data-pubid="_ID_" data-action="pubdetails" target="_self">_DETAILS_</a></p>
         </div>
     </div>
 </div>
