939a76151306b301b0c549bdf15041c338a65503
Marco Di Bella <mdb@marcodibella.de>
2023-09-24 20:14:57 +0200

    Use WP style for control structures

--- includes/classes/class-ajax-loadmore.php
+++ includes/classes/class-ajax-loadmore.php
@@ -39,15 +39,14 @@ abstract class AJAX_LoadMore {
         $filepath = dirname( plugin_dir_path( __FILE__ ) ) . '/templates/' . $filename . '.php';
         $buffer   = '';
 
-        if ( true === file_exists( $filepath ) ) :
+        if ( true === file_exists( $filepath ) ) {
 
             // Read template
             $buffer = file_get_contents( $filepath );
 
             // Remove PHP code and excess spaces
             $buffer = trim( preg_replace( '/<\?php(.*|\n|\r)(\?>)/is', '', $buffer ) );
-
-        endif;
+        }
 
         return $buffer;
     }
@@ -126,7 +125,6 @@ abstract class AJAX_LoadMore {
      */
 
     static function render_dynamic_content( $params ) {
-
         $output = '';
         $posts  = static::get_posts( $params );
 
@@ -134,30 +132,29 @@ abstract class AJAX_LoadMore {
         $offset = ($params['nextpage'] - 1) * $params['show'];
 
 
-        if ( $posts) :
+        if ( $posts) {
 
             // Loop through all posts
-            foreach ( $posts as $post ) :
+            foreach ( $posts as $post ) {
                 $buffer       = static::get_template();
                 $replacements = static::get_replacements( $post );
 
                 $replacements['_NR_'] = $count + $offset;
 
-                foreach ( $replacements as $placeholder => $replacement ) :
+                foreach ( $replacements as $placeholder => $replacement ) {
                     $buffer = str_replace( $placeholder, $replacement, $buffer );
-                endforeach;
+                }
 
                 $count++;
                 $output .= $buffer;
-            endforeach;
+            }
 
 
             // Improve typography (when plugin wp-typography is loaded)
-            if ( class_exists( 'WP_Typography' ) ) :
+            if ( class_exists( 'WP_Typography' ) ) {
                 $output = \WP_Typography::process_title( $output );
-            endif;
-
-        endif;
+            }
+        }
 
         return $output;
     }
@@ -173,9 +170,9 @@ abstract class AJAX_LoadMore {
         $default = static::get_default_params();
         $params  = [];
 
-        foreach ( $default as $key => $value ) :
+        foreach ( $default as $key => $value ) {
             $params[$key] = $_POST[$key];
-        endforeach;
+        }
 
         echo self::render_dynamic_content( $params );
         die();
@@ -196,23 +193,23 @@ abstract class AJAX_LoadMore {
     static function prepare_output( $params, $ajax ) {
         $output = '';
 
-        if ( ! empty( $ajax ) ) :
+        if ( ! empty( $ajax ) ) {
 
             // Correct information for the following page
-            if ( ( 'true' === $params['paged'] ) and ( 1 !== $params['maxpage'] ) ):
+            if ( ( 'true' === $params['paged'] ) and ( 1 !== $params['maxpage'] ) ) {
                 $params['nextpage'] = 2;
-            endif;
+            }
 
             // Generate 'data' values
             $data = '';
 
-            foreach ( $params as $data_key => $data_value ) :
+            foreach ( $params as $data_key => $data_value ) {
                 $data .= sprintf(
                     ' data-%1$s="%2$s"',
                     $data_key,
                     $data_value
                 );
-            endforeach;
+            }
 
 
             // Start rendering
@@ -227,7 +224,7 @@ abstract class AJAX_LoadMore {
             </div>
             <?php
             // Show LoadMore button if required
-            if ( ( 'true' === $params['paged'] ) and ( 1 !== $params['nextpage'] ) ) :
+            if ( ( 'true' === $params['paged'] ) and ( 1 !== $params['nextpage'] ) ) {
             ?>
             <div class="loadmore-action-wrapper">
                 <div class="loadmore-spinner">
@@ -243,13 +240,13 @@ abstract class AJAX_LoadMore {
                 </div>
             </div>
             <?php
-            endif;
+            }
             ?>
             </div>
             <?php
             $output = ob_get_contents();
             ob_end_clean();
-        endif;
+        }
 
         return $output;
     }
