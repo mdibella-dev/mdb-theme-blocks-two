6f1aea8a5a7e657b48766486f80d96e693748b40
Marco Di Bella <mdb@marcodibella.de>
2024-02-24 18:54:16 +0100

    Update ajax-loadmore.js

--- assets/src/js/ajax-loadmore.js
+++ assets/src/js/ajax-loadmore.js
@@ -1,3 +1,55 @@
+const loadmore_buttons = document.querySelectorAll( '.loadmore-button' );
+
+loadmore_buttons.forEach( loadmore_button => {
+    loadmore_button.addEventListener( 'click', function( e ) {
+
+        e.preventDefault();
+
+        // do AJAX
+        const request = new XMLHttpRequest();
+
+        request.onloadstart = function( e ) {
+            console.log( 'onLoadStart' );
+            //lm.children( '.loadmore-action-wrapper' ).find( '.loadmore-button' ).hide();
+            //lm.children( '.loadmore-action-wrapper' ).find( '.loadmore-spinner' ).show();
+        }
+
+        request.onload = function( e ) {
+            if ( request.status == 201 ) {
+                console.log( 'onLoad' );
+                // request.responseText;
+                // lm.children( '.loadmore-content-wrapper' ).find( '.loadmore-content' ).append( jqXHR.responseText );
+                // lm.children( '.loadmore-action-wrapper' ).find( '.loadmore-spinner' ).hide();
+
+                /*
+                nextpage = lm.data( 'nextpage' );
+                maxpage  = lm.data( 'maxpage' );
+
+                if( nextpage != maxpage ) {
+                    lm.data( 'nextpage', nextpage+1 );
+                    lm.children( '.loadmore-action-wrapper' ).find( '.loadmore-button' ).show();
+                } else {
+                    lm.children( '.loadmore-action-wrapper' ).hide();
+                }*/
+            }
+        }
+
+
+        request.open( 'POST', mdb_ajax.ajaxurl, true );
+        request.setRequestHeader( 'Accept', 'text/html' );
+        request.send();
+    } );
+} );
+
+
+
+
+
+
+
+
+
+/*
 jQuery( document ).ready( function( $ ) {
 
     // Listen for a click event on our text input submit button
@@ -45,3 +97,4 @@ jQuery( document ).ready( function( $ ) {
         } );
     } );
 } );
+*/
